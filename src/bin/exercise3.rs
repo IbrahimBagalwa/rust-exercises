3380dc1b8cd61f125c9cae86e461b4bb04929ee2
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-09-17 20:27:59 +0200

    feat: control flow in rust

---
+++ src/bin/exercise3.rs
@@ -0,0 +1,20 @@
+// Topic: Control flow if ... else
+// Program requirements:
+// 
+// * Display a message based on the value of a boolean variable
+// * When the variable is set to true, display "Hello"
+// * When the variable is set to false, display "Goodbye"
+// 
+// Notes:
+// * Use a variable set either to true or false
+// * Use an if..elsee block to determine which message to display
+// * Use the println macro to display the message to the terminal
+
+fn main(){
+    let is_geeting = false;
+    if is_geeting {
+        println!("Hello");
+    }else{
+        println!("Goodbye");
+    }
+}
