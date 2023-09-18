8c732a7d3c0b2d0aea7f80622ec740c585d3d22e
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-09-18 13:03:55 +0200

    feat: function in rust

---
+++ src/bin/exercise1.rs
@@ -0,0 +1,23 @@
+
+// Topic: Functions
+// 
+//  Program requirements:
+//  * Display your first and last name
+// 
+// Notes:
+// * Use function to display your first name
+// * Use funtion to display your last name
+// * Use the println macro to display messages to the terminal
+
+fn first_name() {
+    println!("Ibrahim");
+}
+
+fn last_name(){
+    println!("Bagalwa");
+}
+
+fn main(){
+    first_name();
+    last_name();
+}
