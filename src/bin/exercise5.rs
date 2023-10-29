9e8b5d96391855eda15f1f0477c39e2be283bd94
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2023-10-29 12:58:11 +0200

    feat: exercise 5 recap

--- src/bin/exercise5.rs
+++ src/bin/exercise5.rs
@@ -8,15 +8,35 @@
 // * Use a loop statement
 // * Print the variable with the loop statement
 // * Use break to exit the loop
-
 fn main(){
     let mut i = 1;
-    loop {
-        println!("{i}");
+    loop{
+        println!("{}", i);
         if i == 4{
             break;
         }
         i += 1;
     }
-    println!("Done!");
+    println!("✅ Done");
+    while_loop();
+    println!("✅  Done the While Loop");
 }
+
+fn while_loop(){
+    let mut count = 1;
+    while count < 4{
+        println!("{}", count);
+        count += 1;
+    }
+}
+// fn main(){
+//     let mut i = 1;
+//     loop {
+//         println!("{i}");
+//         if i == 4{
+//             break;
+//         }
+//         i += 1;
+//     }
+//     println!("Done!");
+// }
