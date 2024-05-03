ced3199f6b7420c2e0de180c9f3f172cd473b5ce
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2024-05-03 07:54:00 +0200

    feat: diagnostic attribut 1.78

--- src/bin/diagnostic_attribut.rs
+++ src/bin/diagnostic_attribut.rs
@@ -1,6 +1,13 @@
-trait BeforeDiagnostic<A> {}
+#[diagnostic::on_unimplemented(
+message = "Here is my message for `ImportantTrait<{A}>` is not implemented for `{Self}` type!",
+label = "Missing implementation for `ImportantTrait<{A}>`",
+note = "You need to implement MyTrait for this type to use it here.",
+note = "Note 2"
+)]
+trait ImportantTrait<A> {}
 
-fn use_my_trait(_: impl BeforeDiagnostic<i32>){}
-fn main(){
+fn use_my_trait(_: impl ImportantTrait<i32>) {}
+
+fn main() {
     use_my_trait(String::new());
 }
